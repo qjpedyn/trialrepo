cce540ed4a70bb840c959311cd348d5589a9c2ee
Jaycee <qjpedayan@gmail.com>
2026-09-15 05:23:21 +0800

    commit

--- Sample.py
+++
@@ -1,1 +0,0 @@
-print("Hello World of GitHub")
---
+++ index.py
@@ -0,0 +1,34 @@
+@app.callback(
+    [
+        Output('page_content', 'children')
+    ],
+    [
+        Input('url', 'pathname')
+    ]
+)
+def displaypage (pathname):
+    
+    # This code block extracts the id of the triggered input
+    ctx = dash.callback_context
+    if ctx.triggered:
+        eventid = ctx.triggered[0]['prop_id'].split('.')[0]   
+    else:
+        raise PreventUpdate
+
+        
+    # This code block executes action based on the value of eventid
+    if eventid == 'url':
+        if pathname == '/' or pathname == '/home':
+            returnlayout = "home"
+
+        elif pathname == '/movies':
+            returnlayout = 'moviepage'
+
+        else:
+            returnlayout = 'error404'
+    
+    else: 
+        raise PreventUpdate
+    
+    return [returnlayout]
+    
